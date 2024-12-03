3169bcb4068d00cfcd08be9fae819af71b6f6980
Kaium Al Limon <limon.3cs@gmail.com>
2024-12-03 22:52:36 +0600

    inventory route add

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@
 const express = require('express')
 const app = require('./server')
 const authRoute = require('./features/auth/routes/auth.routes')
+const inventoryRoute = require('./features/inventory/routes/inventory.route')
 const passport = require('./config/passport.config')
 const session = require('express-session')
 const connectDB = require('./config/database.config')
@@ -22,25 +23,30 @@ app.use(express.json())
 // use session
 app.use(
     session({
-      secret: process.env.SESSION_SECRET || 'defaultsecret',
-      resave: false,
-      saveUninitialized: false,
+        secret: process.env.SESSION_SECRET || 'defaultsecret',
+        resave: false,
+        saveUninitialized: false,
     })
-  );
+);
 
-  app.use(passport.initialize());
-  app.use(passport.session());
-  
-  // Connect the Express server to the database
-  connectDB();
-  
-  // Use the auth route
-  app.use('/auth', authRoute);
-  
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Connect the Express server to the database
+connectDB();
+
+// Use the auth route
+app.use('/auth', authRoute);
+
+
+// use the inventory route
+app.use('/inventory', inventoryRoute)
+
+
 
 // start the server
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`)
 })
 
