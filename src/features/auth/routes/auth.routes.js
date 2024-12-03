aaf28e6ca8df091fe58531706130078a6d3cd4cc
Kaium Al Limon <limon.3cs@gmail.com>
2024-12-03 21:33:53 +0600

    router exporting fix

--- src/features/auth/routes/auth.routes.js
+++ src/features/auth/routes/auth.routes.js
@@ -2,7 +2,8 @@
 
 const express = require('express')
 const authController = require('../controllers/auth.controller')
-const createUpload = require('../../../utils/image.upload')
+const createUpload = require('../../../utils/image.upload');
+const { route } = require('../../../server');
 
 // create a router
 const router = express.Router()
@@ -16,3 +17,4 @@ router.post('/register',upload.single('profileImage'), authController.register)
 router.post('/login',authController.login)
 
 
+module.exports  = router
